d7003b88998e01bad9522c63da02949bfa0ad373
turtle601 <turtle601@naver.com>
2025-07-30 18:24:16 +0900

    fix: ai 자동 카테고리 생성 기능 제거 및 한글 입력 시 중복 입력 오류 해결

--- src/features/autoCategorization/categoryForm/categoryForm.ui.tsx
+++ src/features/autoCategorization/categoryForm/categoryForm.ui.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { css } from '@emotion/react';
-import { useQuery } from '@tanstack/react-query';
 
 import { spacer } from '@/shared/config/styles';
 
-import AIIcon from '@/shared/config/assets/ai.svg';
-
 import Tag from '@/shared/ui/tag';
 import Flex from '@/shared/ui/flex';
-import Toggle from '@/shared/ui/toggle';
+
 import Spacer from '@/shared/ui/spacer';
-import SkeletonTag from '@/shared/ui/skeleton/skeletonTag';
+
 import ModalLayer from '@/shared/ui/modalLayer';
 import Button from '@/shared/ui/button';
 import OpenClassifyBookmarkForm from '@/app/modal-router/openClassifyBookmarkForm.ui';
@@ -19,61 +16,22 @@ import { ErrorMessage, Input } from '@/shared/ui/input';
 
 import { useForm } from '@/shared/hooks/useForm';
 import { useArray } from '@/shared/hooks/useArray';
-import { useBoolean } from '@/shared/hooks/useBoolean';
 
-import { categoryService } from '@/entities/category';
 import { useClassifyAIBookmarks } from '@/entities/classify';
 
 import type { FormRefValueType } from '@/shared/hooks/useForm';
 import type { ICategory } from '@/entities/category/category.type';
-
-const SkeletonCategoryList = () => {
-  return (
-    <Flex
-      as="ul"
-      etcStyles={{
-        marginTop: spacer['spacing2.5'],
-        height: '120px',
-      }}
-      gap={spacer['spacing2.5']}
-    >
-      <SkeletonTag />
-    </Flex>
-  );
-};
 
 const CategoryForm: React.FC = () => {
   const { errorMessage, register, handleOnAction } = useForm();
 
   const { mutate: classifyAIBookmarks } = useClassifyAIBookmarks();
 
-  const { value: isAI, toggle: toggleAI } = useBoolean(true);
-
-  const { isLoading: isLoadingCategories, data: fetchedCategories } = useQuery(
-    categoryService.queryOptions(),
-  );
-
   const {
     value: categories,
     unshift: addCategory,
     remove: removeCategory,
   } = useArray<ICategory>([]);
-
-  useEffect(() => {
-    if (fetchedCategories) {
-      if (isAI) {
-        fetchedCategories.forEach((category) => {
-          addCategory(category);
-        });
-      } else {
-        fetchedCategories.forEach((category) => {
-          removeCategory(category.id);
-        });
-      }
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchedCategories, isAI]);
 
   const isDisabled = categories.length === 0;
 
@@ -83,42 +41,44 @@ const CategoryForm: React.FC = () => {
     if (e.key === 'Enter') {
       e.stopPropagation();
 
-      const validateLength = (formRefValue: FormRefValueType) => {
-        return formRefValue['category-form'].element.value.length > 0;
-      };
-
-      const validateNonDuplicate = (formRefValue: FormRefValueType) => {
-        const inputValue = formRefValue['category-form'].element.value;
-
-        const tagList = Object.values(formRefValue).filter((formRefValue) => {
-          return (
-            formRefValue.element && formRefValue.element.name === 'category-tag'
-          );
-        });
-
-        const isDuplicate = tagList.some((tag) => {
-          return tag.element.value === inputValue;
-        });
-
-        return !isDuplicate;
-      };
-
-      const action = (formRefValue: FormRefValueType) => {
-        addCategory({
-          id: formRefValue['category-form'].element.value,
-          text: formRefValue['category-form'].element.value,
-        });
-      };
+      if (e.nativeEvent.isComposing) {
+        return;
+      }
 
       handleOnAction({
-        action,
+        action: (formRefValue: FormRefValueType) => {
+          addCategory({
+            id: formRefValue['category-form'].element.value,
+            text: formRefValue['category-form'].element.value,
+          });
+        },
+
         wholeValidate: [
           {
-            fn: validateLength,
+            fn: (formRefValue: FormRefValueType) => {
+              return formRefValue['category-form'].element.value.length > 0;
+            },
             errorMessage: '최소 한 글자 이상의 카테고리를 만들어주세요.',
           },
           {
-            fn: validateNonDuplicate,
+            fn: (formRefValue: FormRefValueType) => {
+              const inputValue = formRefValue['category-form'].element.value;
+
+              const tagList = Object.values(formRefValue).filter(
+                (formRefValue) => {
+                  return (
+                    formRefValue.element &&
+                    formRefValue.element.name === 'category-tag'
+                  );
+                },
+              );
+
+              const isDuplicate = tagList.some((tag) => {
+                return tag.element.value === inputValue;
+              });
+
+              return !isDuplicate;
+            },
             errorMessage: '중복되는 카테고리는 만들 수 없습니다.',
           },
         ],
@@ -142,12 +102,8 @@ const CategoryForm: React.FC = () => {
           type="text"
           kind={'outline'}
           placeholder="카테고리를 입력하세요"
-          onKeyUp={handleInputEnter}
+          onKeyDown={handleInputEnter}
         />
-        <Flex align={'center'} gap={spacer.spacing2}>
-          <AIIcon />
-          <Toggle isChecked={isAI} onClick={toggleAI} />
-        </Flex>
       </Flex>
       <Spacer direction="vertical" space={spacer['spacing2.5']} />
       <div
@@ -165,7 +121,6 @@ const CategoryForm: React.FC = () => {
             overflow: 'auto',
           }}
         >
-          {isLoadingCategories && <SkeletonCategoryList />}
           {categories.map((category) => {
             return (
               <Tag
