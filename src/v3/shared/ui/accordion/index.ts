8453d74a63123c84d39b4906d6fddaca6d319568
turtle601 <turtle601@naver.com>
2025-08-24 18:34:53 +0900

    ♻️ refactor: accordion 컴포넌트 파일 및 폴더 위치 이동

---
+++ src/v3/shared/ui/accordion/index.ts
@@ -0,0 +1,8 @@
+import { Provider, Panel, Icon, Button } from './part';
+
+export const Accordion = {
+  Provider,
+  Panel,
+  Icon,
+  Button,
+} as const;
---
+++ src/v3/shared/ui/accordion/part/button.tsx
@@ -0,0 +1,49 @@
+import React, { ComponentPropsWithoutRef, useCallback } from 'react';
+
+import { css, type CSSObject } from '@emotion/react';
+
+import Center from '@/v3/shared/ui/layout/center';
+
+import { useAccordionActionContext, useAccordionContext } from '../model';
+
+export interface IButtonProps extends ComponentPropsWithoutRef<'button'> {
+  id: string;
+  children: React.ReactNode;
+  etcStyles?: CSSObject;
+}
+
+const Button: React.FC<IButtonProps> = ({
+  id,
+  children,
+  etcStyles,
+  ...attribute
+}) => {
+  const buttonId = Number(id);
+
+  const { selectedIdSet } = useAccordionContext();
+  const { closeAccordion, openAccordion } = useAccordionActionContext();
+
+  const toggle = useCallback(() => {
+    if (selectedIdSet.has(buttonId)) {
+      closeAccordion(buttonId);
+      return;
+    }
+
+    openAccordion(buttonId);
+  }, [buttonId, closeAccordion, openAccordion, selectedIdSet]);
+
+  return (
+    <Center
+      as="button"
+      onClick={toggle}
+      css={css({
+        ...etcStyles,
+      })}
+      {...attribute}
+    >
+      {children}
+    </Center>
+  );
+};
+
+export default Button;
