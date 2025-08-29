1d5df75ae7c281e0b718538bf9917e8a41e77464
turtle601 <turtle601@naver.com>
2025-08-29 16:36:54 +0900

    🐛 fix: 중복 스크롤 바 생성 오류 해결

--- src/v3/features/edit/bookmarkTree/bookmarkTree.tsx
+++ src/v3/features/edit/bookmarkTree/bookmarkTree.tsx
@@ -30,7 +30,6 @@ function BookmarkTree() {
       css={css({
         width: '100%',
         height: '100%',
-        overflowY: 'auto',
       })}
     >
       {rootBookmarkChildren.map((bookmark) => (
