eefe52f0cdae607a66aa84db025212bf8ee0aca6
turtle601 <turtle601@naver.com>
2025-08-29 11:54:08 +0900

    🐛 fix: 스크롤 된 링크의 경우 edit dropdown 이 예상된 위치에 오지 않는 버그 해결

--- src/v3/features/edit/bookmarkTree/ui/bookmarkEditButton.tsx
+++ src/v3/features/edit/bookmarkTree/ui/bookmarkEditButton.tsx
@@ -4,6 +4,7 @@ import { color } from '@/v3/shared/styles';
 
 import { DropDown } from '@/v3/shared/ui/dropdown';
 import Center from '@/v3/shared/ui/layout/center';
+import { useDropDownContext } from '../../../../shared/ui/dropdown/model/context';
 
 interface IBookmarkEditButtonProps {
   options: {
@@ -12,84 +13,95 @@ interface IBookmarkEditButtonProps {
   }[];
 }
 
-function BookmarkEditButton({ options }: IBookmarkEditButtonProps) {
+function BookmarkEditButtonTrigger({ options }: IBookmarkEditButtonProps) {
+  const { isOpen } = useDropDownContext();
+
   const [optionsPosition, setOptionsPosition] = useState({
     x: 0,
     y: 0,
   });
 
-  const triggerRef = useCallback((el: HTMLDivElement) => {
-    if (el) {
-      setTimeout(() => {
-        setOptionsPosition({
-          x: el.getBoundingClientRect().left,
-          y: el.getBoundingClientRect().bottom,
-        });
-      }, 0);
-    }
-  }, []);
+  const triggerRef = useCallback(
+    (el: HTMLDivElement) => {
+      if (el && isOpen) {
+        setTimeout(() => {
+          setOptionsPosition({
+            x: el.getBoundingClientRect().left,
+            y: el.getBoundingClientRect().bottom,
+          });
+        }, 0);
+      }
+    },
+    [isOpen],
+  );
 
   return (
-    <DropDown.Provider>
-      <div
-        css={css({
-          position: 'relative',
-        })}
+    <div
+      css={css({
+        position: 'relative',
+      })}
+    >
+      <DropDown.Trigger>
+        <div ref={triggerRef}>
+          <Center
+            as="button"
+            data-bookmark-edit-button
+            etcStyles={{
+              width: '12px',
+              height: '100%',
+              opacity: 0,
+              borderRadius: '2px',
+              transition: 'opacity 0.2s ease',
+              background: 'transparent',
+              '&:hover': {
+                background: color.slate['100'],
+                opacity: 1,
+              },
+            }}
+          >
+            ⋮
+          </Center>
+        </div>
+      </DropDown.Trigger>
+      <DropDown.Options
+        etcStyles={{
+          position: 'fixed',
+          top: `${optionsPosition.y}px`,
+          left: `${optionsPosition.x - 148}px`,
+          width: '160px',
+          zIndex: 99999999,
+          background: color.white,
+          borderRadius: '4px',
+          boxShadow: '0 2px 8px rgba(0, 0, 0, 0.04)',
+          border: `1px solid ${color.slate['200']}`,
+          padding: '4px 8px',
+          'li + li': {
+            borderTop: `1px solid ${color.slate['200']}`,
+          },
+        }}
       >
-        <DropDown.Trigger>
-          <div ref={triggerRef}>
-            <Center
-              as="button"
-              data-bookmark-edit-button
+        {options.map((option) => {
+          return (
+            <DropDown.Option
+              key={option.label}
+              onClick={option.action}
               etcStyles={{
-                width: '12px',
-                height: '100%',
-                opacity: 0,
-                borderRadius: '2px',
-                transition: 'opacity 0.2s ease',
-                background: 'transparent',
-                '&:hover': {
-                  background: color.slate['100'],
-                  opacity: 1,
-                },
+                padding: '4px 0',
               }}
             >
-              ⋮
-            </Center>
-          </div>
-        </DropDown.Trigger>
-        <DropDown.Options
-          etcStyles={{
-            position: 'fixed',
-            top: `${optionsPosition.y}px`,
-            left: `${optionsPosition.x - 148}px`,
-            width: '160px',
-            zIndex: 99999999,
-            background: color.white,
-            borderRadius: '4px',
-            boxShadow: '0 2px 8px rgba(0, 0, 0, 0.04)',
-            border: `1px solid ${color.slate['200']}`,
-            padding: '4px 8px',
-            'li + li': {
-              borderTop: `1px solid ${color.slate['200']}`,
-            },
-          }}
-        >
-          {options.map((option) => {
-            return (
-              <DropDown.Option
-                key={option.label}
-                onClick={option.action}
-                etcStyles={{
-                  padding: '4px 0',
-                }}
-              >
-                <p>{option.label}</p>
-              </DropDown.Option>
-            );
-          })}
-        </DropDown.Options>
-      </div>
+              <p>{option.label}</p>
+            </DropDown.Option>
+          );
+        })}
+      </DropDown.Options>
+    </div>
+  );
+}
+
+function BookmarkEditButton({ options }: IBookmarkEditButtonProps) {
+  return (
+    <DropDown.Provider>
+      <BookmarkEditButtonTrigger options={options} />
     </DropDown.Provider>
   );
 }
