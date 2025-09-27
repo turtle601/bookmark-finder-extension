9838e630a1ec85792faf0a7a89f24e52cd770cb3
turtle601 <turtle601@naver.com>
2025-09-28 03:54:56 +0900

    ✨ feat: chunkArray 유틸함수 구현

---
+++ apps/chrome-extension/src/v3/shared/utils/optimize/chunk.ts
@@ -0,0 +1,9 @@
+export const chunkArray = <T>(array: T[], chunkSize: number = 10): T[][] => {
+  const chunks: T[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+
+  return chunks;
+};
